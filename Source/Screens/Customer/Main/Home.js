92975c502e5b0100c2c1f0c6b452a975caa339aa
pravin.sphinx <vaishnavi@sphinx-solution.com>
2022-09-15 18:18:37 +0530

    [SH] - changes in home screen

--- Source/Screens/Customer/Main/Home.js
+++ Source/Screens/Customer/Main/Home.js
@@ -97,7 +97,7 @@ const Home = ({ navigation, props, route }) => {
       .then(res => {
         console.log('list of stores=====>>>>', res.data.list);
         setStoreList(res.data.list)
-        calculateDistance(global.latitude, global.longitude, storeList.latitude, storeList.longitude)
+        // calculateDistance(global.latitude, global.longitude, storeList.latitude, storeList.longitude)
         setLoading(false)
       })
       .catch(e => {
@@ -107,29 +107,29 @@ const Home = ({ navigation, props, route }) => {
   }
 
 
-  const calculateDistance = (lattitude1, longittude1, lattitude2, longittude2) => {
+  // const calculateDistance = (lattitude1, longittude1, lattitude2, longittude2) => {
 
-    const toRadian = n => (n * Math.PI) / 180
+  //   const toRadian = n => (n * Math.PI) / 180
 
-    let lat2 = lattitude2
-    let lon2 = longittude2
-    let lat1 = lattitude1
-    let lon1 = longittude1
+  //   let lat2 = lattitude2
+  //   let lon2 = longittude2
+  //   let lat1 = lattitude1
+  //   let lon1 = longittude1
 
-    console.log(lat1, lon1 + "===" + lat2, lon2)
-    let R = 6371  // km
-    let x1 = lat2 - lat1
-    let dLat = toRadian(x1)
-    let x2 = lon2 - lon1
-    let dLon = toRadian(x2)
-    let a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(toRadian(lat1)) * Math.cos(toRadian(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2)
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-    let d = R * c
-    console.log("distance==?", d)
-    return d
-  }
+  //   console.log(lat1, lon1 + "===" + lat2, lon2)
+  //   let R = 6371  // km
+  //   let x1 = lat2 - lat1
+  //   let dLat = toRadian(x1)
+  //   let x2 = lon2 - lon1
+  //   let dLon = toRadian(x2)
+  //   let a =
+  //     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+  //     Math.cos(toRadian(lat1)) * Math.cos(toRadian(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2)
+  //   let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  //   let d = R * c
+  //   console.log("distance==?", d)
+  //   return d
+  // }
 
   const _onSalonCancel = async () => {
     dispatch(deleteSalon(updatedName.fav))
